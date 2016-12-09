c18ae1d21b3b79de2b9db88be10eabcfe908ac6b
Johnny Choo <jchoo156@gmail.com>
2016-12-08 23:18:14 -0800

    parse json to get id of summoner

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,22 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+
+  private
+
+  def base_uri
+    'https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/'
+  end
+
+  def api_key
+    '?api_key=45ff6a4c-f994-420d-a018-189db4cb494e'
+  end
+
+  def summoner_search_start
+    'https://na.api.pvp.net/api/lol/na/v1.3/stats/by-summoner/'
+  end
+
+  def summoner_search_tail
+    '/summary?season=SEASON2016'
+  end
 end
--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -2,21 +2,20 @@ require 'httparty'
 
 class StatsController < ApplicationController
   include HTTParty
-  BASE_URI = 'https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/'
 
   def index
-    @summoner_name = params[:summoner_name]
-    @response = HTTParty.get(BASE_URI + 'choobie' + api_key)
+    response = HTTParty.get(base_uri + 'choobie' + api_key)
+    @body = JSON.parse(response.body)
+    @id = @body["choobie"]["id"]
   end
 
   def show
-    @summoner_name = params[:summoner_name]
-    @response = HTTParty.get(BASE_URI + "#{@summoner_name}" + api_key)
+    summoner_name = params[:summoner_name]
+    response = HTTParty.get(base_uri + "#{summoner_name}" + api_key)
+    @body = JSON.parse(response.body)
+    id = @body["#{summoner_name}"]["id"]
+    
+
   end
 
-  private
-
-  def api_key
-    '?api_key=45ff6a4c-f994-420d-a018-189db4cb494e'
-  end
 end
