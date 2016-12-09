8d1977dc405c1fea4ea9c683c1dfd4ee621060e5
Johnny Choo <jchoo156@gmail.com>
2016-12-08 22:53:25 -0800

    add league api to application and properly make calls

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,7 +1,22 @@
+require 'httparty'
+
 class StatsController < ApplicationController
+  include HTTParty
+  BASE_URI = 'https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/'
+
   def index
+    @summoner_name = params[:summoner_name]
+    @response = HTTParty.get(BASE_URI + 'choobie' + api_key)
   end
 
   def show
+    @summoner_name = params[:summoner_name]
+    @response = HTTParty.get(BASE_URI + "#{@summoner_name}" + api_key)
+  end
+
+  private
+
+  def api_key
+    '?api_key=45ff6a4c-f994-420d-a018-189db4cb494e'
   end
 end
