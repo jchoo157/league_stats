7d6a7f9598f4194e5dec43c9c7c21355c839f19c
Johnny Choo <jchoo156@gmail.com>
2016-12-11 16:42:54 -0800

    display KDA

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
 
   private
 
+  # Summoner stats
   def base_uri(summoner_name)
     'https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/' + summoner_name + '?api_key=' + ENV['LEAGUE_API']
   end
@@ -16,4 +17,12 @@ class ApplicationController < ActionController::Base
     'https://na.api.pvp.net/api/lol/na/v1.3/game/by-summoner/' + summoner_id + '/recent?api_key=' + ENV['LEAGUE_API']
   end
 
+  # Champions
+  def champions_url
+    "https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion?api_key=" + ENV['LEAGUE_API']
+  end
+
+  def champions_json
+    @champions = JSON.parse(champions_url)
+  end
 end
