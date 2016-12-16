621ad71beb427db3466c01b8760930f7dcc8c42d
Johnny Choo <jchoo156@gmail.com>
2016-12-15 16:24:08 -0800

    parse json to get summoner name displayed on match history page

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -11,11 +11,11 @@ class StatsController < ApplicationController
 
   def show
     # Once the show page receives a valid summoner name it will start off getting their id
-    summoner_name = params[:summoner_name]
-    response = HTTParty.get(base_uri(summoner_name))
+    @summoner_name = params[:summoner_name].downcase
+    response = HTTParty.get(base_uri(@summoner_name))
     @body = JSON.parse(response.body)
     # Right here we get the id. Now we can use that id to get stats/games/etc
-    id = @body[summoner_name]["id"].to_s
+    id = @body[@summoner_name]["id"].to_s
     # getting summoner_stats_search from application controller
     stats_response = HTTParty.get(summoner_stats_search(id))
     @stats_body = JSON.parse(stats_response.body)
