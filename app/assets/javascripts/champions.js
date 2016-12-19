8c1f991b1e1c19ef9eed44a2c02715a9b7bf6bd6
Johnny Choo <jchoo156@gmail.com>
2016-12-18 22:08:23 -0800

    add hover effect on champ icon to display some information

--- app/assets/javascripts/champions.js
+++ app/assets/javascripts/champions.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
-  
-})
+  champIconHover();
+});
+
+function champIconHover() {
+  $('.champions').on('mouseenter', '.champ-icon', function(e) {
+    e.preventDefault();
+    $(this).parent().parent().find('.champ-info-box').show();
+  });
+
+  $('.champions').on('mouseleave', '.champ-icon', function(e) {
+    e.preventDefault();
+    $(this).parent().parent().find('.champ-info-box').hide();
+  });
+};
